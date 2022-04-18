5129fbf2c80d6576605bc649995d57677cbe5b4a
Pasha Baran <pashabaran1207@gmail.com>
2022-04-18 23:11:52 +0300

    Code improvements?

--- server.go
+++ server.go
@@ -1,12 +1,13 @@
 package main
+import ("fmt""time")
 
-import (
-	"fmt"
-	"time"
-)
+Some bad code...
 
 func main() {
 	const RFC3339 = "2006-01-02T15:04:05Z07:00"
 	now := time.Now().Format(RFC3339)
+And here...
 	fmt.Println(now)
+
+
 }
