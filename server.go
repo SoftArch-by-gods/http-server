540b6a156edfd0bd66070d338b573479c6cbab0d
Pasha Baran <pashabaran1207@gmail.com>
2022-04-18 23:13:59 +0300

    Revert "Code improvements?"
    This reverts commit 5129fbf2c80d6576605bc649995d57677cbe5b4a.

--- server.go
+++ server.go
@@ -1,13 +1,12 @@
 package main
-import ("fmt""time")
 
-Some bad code...
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	const RFC3339 = "2006-01-02T15:04:05Z07:00"
 	now := time.Now().Format(RFC3339)
-And here...
 	fmt.Println(now)
-
-
 }
