fe0a03cf2c4abfce223624c14c83b196308a5241
Pasha Baran <danil.bezus.db@gmail.com>
2022-05-02 13:09:11 +0300

    Add "/time" section

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	const RFC3339 = "2006-01-02T15:04:05Z07:00"
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now().Format(RFC3339)
 		fmt.Println(now)
 	})
