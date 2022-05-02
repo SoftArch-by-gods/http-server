76d5596c5564196a203595fe0d0da4221fa359ab
Pasha Baran <pashabaran1207@gmail.com>
2022-05-02 13:15:02 +0300

    Revert "Correct of displaying time from console to browser"
    This reverts commit 9cfdec1422d750100b184517057831d4bcf0b763.

--- server.go
+++ server.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"time"
 )
 
 func main() {
 	const RFC3339 = "2006-01-02T15:04:05Z07:00"
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		now := time.Now().Format(RFC3339)
-		fmt.Fprintf(w, now)
-	})
-	http.ListenAndServe(":8795", nil)
+	now := time.Now().Format(RFC3339)
+	fmt.Println(now)
 }
