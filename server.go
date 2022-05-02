94e476793d260a8b8c8a216037e81f7cf5d0ce09
grefle <71408140+grefle@users.noreply.github.com>
2022-05-03 00:35:20 +0300

    Update server.go

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -12,10 +13,14 @@ type Date struct {
 
 func main() {
 	const RFC3339 = "2006-01-02T15:04:05Z07:00"
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now().Format(RFC3339)
 		inStruct := Date{now}
-		fmt.Fprintf(w, string(inStruct))
+
+		data, _ := json.MarshalIndent(inStruct, "", "  ")
+		result := string(data)
+		fmt.Fprintf(w, result)
 	})
 	http.ListenAndServe(":8795", nil)
 }
