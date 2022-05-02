9cfdec1422d750100b184517057831d4bcf0b763
Pasha Baran <pashabaran1207@gmail.com>
2022-05-02 13:12:20 +0300

    Correct of displaying time from console to browser

--- server.go
+++ server.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
 func main() {
 	const RFC3339 = "2006-01-02T15:04:05Z07:00"
-	now := time.Now().Format(RFC3339)
-	fmt.Println(now)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		now := time.Now().Format(RFC3339)
+		fmt.Fprintf(w, now)
+	})
+	http.ListenAndServe(":8795", nil)
 }
