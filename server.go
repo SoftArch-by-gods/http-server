a9fe81ec91b075fa8f0c4a83079977332837fca6
Pasha Baran <danil.bezus.db@gmail.com>
2022-05-02 23:38:10 +0300

    Some json fixes

--- server.go
+++ server.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 )
 
+type Date struct {
+	Time string `json:"time"`
+}
+
 func main() {
 	const RFC3339 = "2006-01-02T15:04:05Z07:00"
 	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now().Format(RFC3339)
-		fmt.Println(now)
+		inStruct := Date{now}
+		data, _ := json.MarshalIndent(inStruct, "", "\t")
+		fmt.Fprintf(w, string(data))
 	})
 	http.ListenAndServe(":8795", nil)
 }
