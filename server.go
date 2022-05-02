42440cd78f49d4cb895e3b38493eb2270ec922ad
grefle <timesetday61@gmail.com>
2022-05-02 18:27:11 +0300

    Update server.go
    Edit structure

--- server.go
+++ server.go
@@ -6,11 +6,16 @@ import (
 	"time"
 )
 
+type Date struct {
+	Time string `json:"time"`
+}
+
 func main() {
 	const RFC3339 = "2006-01-02T15:04:05Z07:00"
-	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now().Format(RFC3339)
-		fmt.Println(now)
+		inStruct := Date{now}
+		fmt.Fprintf(w, string(inStruct))
 	})
 	http.ListenAndServe(":8795", nil)
 }
